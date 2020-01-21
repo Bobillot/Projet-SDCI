c7963e32980989cee09925a8b0ebedbae8d35710
Bobillot <37578379+Bobillot@users.noreply.github.com>
2020-01-21 16:35:13 +0100

    Update pinger.js

--- pinger.js
+++ pinger.js
@@ -1,10 +1,8 @@
 var ping = require('ping');
 
-const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
-var latencies = [];
-var mean = 0;
-//var rank = 0;
+var lat = 0;
+
 var host = '10.0.0.201';
 
 var express = require('express')
@@ -13,28 +11,20 @@ var LOCAL_ENDPOINT = {IP : 'localhost', PORT : 8989, NAME : 'VnfMonitor'};
 
 app.get('/latency', function(req, res) 
 {
+    var d = new Date();
+    var start = d.getTime();
     for (i = 0; i < 100; i++) 
     {
-        latencies.push(0);
-        var d = new Date();
-        var start = d.getTime();
         ping.sys.probe(host, function(isAlive)
         {
             var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
-            //console.log(msg);
         });
-        var d = new Date();
-        var end = d.getTime();
-
-        latency = end-start;
-        latencies[i]=latency;
     }
-    mean = latencies.reduce(reducer);
-    //mean = mean/100;
-    console.log('sent ' + mean);
-    resObj = [mean];
+    var d = new Date();
+    var end = d.getTime();
+    lat=end-start
+    resObj = [lat];
     res.send(resObj);
-    latencies = []
 });
 console.log("Start VNF Monitoring");
 app.listen(LOCAL_ENDPOINT.PORT , function () {
